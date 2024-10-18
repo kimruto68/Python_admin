0c6cf40cd62880eddfc59fba086d75f5d1321316
kimruto68 <kimruto68@gmail.com>
2024-10-18 10:13:51 +0300

    New Files added

---
+++ Roll_Dice.py
@@ -0,0 +1,11 @@
+people=["Peter", "Evans", "Martin"]
+
+def happy_birthday():
+        print("Happy birthday to you,")
+        print("Happy birthday to you,")
+        print(f"Happy birthday dear {name},")
+        print("Happy birthday to you,")
+name = input("Please enter the name: ")
+happy_birthday()
+
+
